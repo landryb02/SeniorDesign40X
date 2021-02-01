b65edee4a88c5f0a0482a2cd44dc768efacd6527
FrankieCook <nfcook097@gmail.com>
2021-02-01 13:37:01 -0600

    Mouse Over Links (v2)
    I am using a library for javaScript called jQuery. It helps tremedously with moving elements around in a webpage. I am using it to move the popup box.

--- CapstoneProject/mouse_link/mouse_link.js
+++ CapstoneProject/mouse_link/mouse_link.js
@@ -1,4 +1,15 @@
-function toggle(ele) {
-    var cont = document.getElementById('cont');
-    cont.style.display = cont.style.display == 'none' ? 'block' : 'none';
-}
+$(document).ready(function(){
+  $("a").hover(function(){
+    $(this).css("background-color", "yellow");
+    var $PosTop = $(this).offset().top;
+    var $PosLeft = $(this).offset().left;
+
+    console.log($PosTop);
+
+    $(".hide").css({top: $PosTop+20, left: $PosLeft+100, position:'absolute'});
+    $(".hide").show()
+    }, function(){
+    $(this).css("background-color", "white");
+    $(".hide").hide()
+  });
+});
