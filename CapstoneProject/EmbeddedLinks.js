02e00aa667eb91f24018c30762b1b042a0097d11
Landry Baudouin <37718566+landryb02@users.noreply.github.com>
2021-02-10 13:21:56 -0600

    Update EmbeddedLinks.js
    Removed trying to check the link in the popup.  They should never showup there anyway.  All output can be viewed in the console output

--- CapstoneProject/EmbeddedLinks.js
+++ CapstoneProject/EmbeddedLinks.js
@@ -10,7 +10,3 @@ console.log(arr);
 //arr is now an array of all the href attributes from the anchors in the page
 //document.links grabs all of the links and then you must loop through,
 //specifically getting the href valued links
-
-console.log("This is the first element of arr: " + arr[0]);
-document.getElementById("embeddedlink").innerHTML = ("<p>"+arr[0]+"</p><br>");
-
